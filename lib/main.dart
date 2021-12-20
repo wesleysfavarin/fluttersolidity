b136fa9b4703e5ddd64348034b8912ef04f0285a
Wesley Favarin <wesleyfavarin@MacBook-Pro-de-Wesley.local>
2021-12-19 21:52:50 -0300

    Flutter Solidity smart contract hash token

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:fluttersolidity/splash.dart';
+void main() {
+  // Firebase.initializeApp();
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:SplashScreen()
+    );
+  }
+}
